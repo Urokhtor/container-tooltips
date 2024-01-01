af3d2780505bc89d2c24e40502a9f07db0228052
Urokhtor <urokhtor1@gmail.com>
2024-01-02 00:05:42 +0200

    Add initial configurations

---
+++ src/client/kotlin/io/urokhtor/minecraft/ContainertooltipsClient.kt
@@ -0,0 +1,9 @@
+package io.urokhtor.minecraft
+
+import net.fabricmc.api.ClientModInitializer
+
+object ContainertooltipsClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
