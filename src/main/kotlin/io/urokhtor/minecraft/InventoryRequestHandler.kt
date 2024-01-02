ae973b9511ffdf4ce4d5eed24fa21ed967b42512
Urokhtor <urokhtor1@gmail.com>
2024-01-02 10:27:26 +0200

    Extract the nbt keys to constants

--- src/main/kotlin/io/urokhtor/minecraft/InventoryRequestHandler.kt
+++ src/main/kotlin/io/urokhtor/minecraft/InventoryRequestHandler.kt
@@ -13,6 +13,9 @@ import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.collection.DefaultedList
 import net.minecraft.util.math.BlockPos
 
+private const val MAX_SIZE = "MaxSize"
+private const val ITEMS = "Items"
+
 class InventoryRequestHandler {
 
     fun createResponse(player: ServerPlayerEntity, blockPosition: BlockPos): NbtCompound? {
@@ -48,22 +51,22 @@ class InventoryRequestHandler {
         }
 
         val nbtCompound = NbtCompound()
-        nbtCompound.put("MaxSize", NbtInt.of(inventory.size()))
+        nbtCompound.put(MAX_SIZE, NbtInt.of(inventory.size()))
         Inventories.writeNbt(nbtCompound, defaultedList)
         return nbtCompound
     }
 
     private fun readGenericInventory(blockEntity: LootableContainerBlockEntity): NbtCompound? {
         val nbtCompound = blockEntity.createNbt()
-        nbtCompound.put("MaxSize", NbtInt.of(blockEntity.size()))
+        nbtCompound.put(MAX_SIZE, NbtInt.of(blockEntity.size()))
         return nbtCompound
     }
 
     private fun readEnderChestInventory(player: ServerPlayerEntity): NbtCompound {
         val nbtList = player.enderChestInventory.toNbtList()
         val nbtCompound = NbtCompound()
-        nbtCompound.put("Items", nbtList)
-        nbtCompound.put("MaxSize", NbtInt.of(player.enderChestInventory.size()))
+        nbtCompound.put(ITEMS, nbtList)
+        nbtCompound.put(MAX_SIZE, NbtInt.of(player.enderChestInventory.size()))
         return nbtCompound
     }
 }
