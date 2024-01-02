f3962e31cbe7da0fc09a0655869225a91b93ed57
Urokhtor <urokhtor1@gmail.com>
2024-01-02 10:18:21 +0200

    Try to make the code more understandable

--- src/main/kotlin/io/urokhtor/minecraft/InventoryRequestHandler.kt
+++ src/main/kotlin/io/urokhtor/minecraft/InventoryRequestHandler.kt
@@ -20,37 +20,57 @@ class InventoryRequestHandler {
 
         return if (blockEntity != null && blockEntity is LootableContainerBlockEntity) {
             if (blockEntity is ChestBlockEntity) {
-                val blockState = player.serverWorld.getBlockState(blockPosition)
-                val inventory = ChestBlock.getInventory(
-                    blockState.block as ChestBlock,
-                    blockState,
-                    player.serverWorld,
-                    blockPosition,
-                    true
-                )
-
-                val defaultedList = DefaultedList.ofSize(inventory!!.size(), ItemStack.EMPTY);
-                IntRange(0, inventory.size() - 1).map { slot ->
-                    defaultedList.set(slot, inventory.getStack(slot))
-                }
-
-                val nbtCompound = NbtCompound()
-                nbtCompound.put("MaxSize", NbtInt.of(inventory.size()))
-                Inventories.writeNbt(nbtCompound, defaultedList)
-                nbtCompound
+                readChestInventory(player, blockPosition)
             } else {
-                val nbtCompound = blockEntity.createNbt()
-                nbtCompound.put("MaxSize", NbtInt.of(blockEntity.size()))
-                nbtCompound
+                readGenericInventory(blockEntity)
             }
         } else if (blockEntity is EnderChestBlockEntity) {
-            val nbtList = player.enderChestInventory.toNbtList()
-            val nbtCompound = NbtCompound()
-            nbtCompound.put("Items", nbtList)
-            nbtCompound.put("MaxSize", NbtInt.of(player.enderChestInventory.size()))
-            nbtCompound
+            readEnderChestInventory(player)
         } else {
             null
         }
+    }
+
+    /**
+     * Reads contents of a chest container. Double chests are supported by using [ChestBlock.getInventory]. They are
+     * also the reason why [readGenericInventory] does not work for chests, because each side of a double chest has a
+     * separate inventory. Without this, the client would get only one half of double chest's inventory.
+     */
+    private fun readChestInventory(
+        player: ServerPlayerEntity,
+        blockPosition: BlockPos
+    ): NbtCompound {
+        val blockState = player.serverWorld.getBlockState(blockPosition)
+        val inventory = ChestBlock.getInventory(
+            blockState.block as ChestBlock,
+            blockState,
+            player.serverWorld,
+            blockPosition,
+            true
+        )
+
+        val defaultedList = DefaultedList.ofSize(inventory!!.size(), ItemStack.EMPTY);
+        IntRange(0, inventory.size() - 1).map { slot ->
+            defaultedList.set(slot, inventory.getStack(slot))
+        }
+
+        val nbtCompound = NbtCompound()
+        nbtCompound.put("MaxSize", NbtInt.of(inventory.size()))
+        Inventories.writeNbt(nbtCompound, defaultedList)
+        return nbtCompound
+    }
+
+    private fun readGenericInventory(blockEntity: LootableContainerBlockEntity): NbtCompound? {
+        val nbtCompound = blockEntity.createNbt()
+        nbtCompound.put("MaxSize", NbtInt.of(blockEntity.size()))
+        return nbtCompound
+    }
+
+    private fun readEnderChestInventory(player: ServerPlayerEntity): NbtCompound {
+        val nbtList = player.enderChestInventory.toNbtList()
+        val nbtCompound = NbtCompound()
+        nbtCompound.put("Items", nbtList)
+        nbtCompound.put("MaxSize", NbtInt.of(player.enderChestInventory.size()))
+        return nbtCompound
     }
 }
