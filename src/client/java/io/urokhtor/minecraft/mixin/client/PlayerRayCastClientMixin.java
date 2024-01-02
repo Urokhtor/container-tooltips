96e39bae129404a0f70b6f7e4627c65fbcd8d9c2
Urokhtor <urokhtor1@gmail.com>
2024-01-02 09:58:44 +0200

    Request container's contents from the server
    The request is sent if player moves their focus on a container, and we have not yet requested its contents. Currently, this only supports LootableContainerBlockEntity containers and Ender Chests. Furnaces are ignored.

--- src/client/java/io/urokhtor/minecraft/mixin/client/PlayerRayCastClientMixin.java
+++ src/client/java/io/urokhtor/minecraft/mixin/client/PlayerRayCastClientMixin.java
@@ -1,15 +1,53 @@
 package io.urokhtor.minecraft.mixin.client;
 
-import net.minecraft.client.MinecraftClient;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.EnderChestBlockEntity;
+import net.minecraft.block.entity.LootableContainerBlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftClient.class)
-public class PlayerRayCastClientMixin {
-	@Inject(at = @At("HEAD"), method = "run")
-	private void run(CallbackInfo info) {
-		// This code is injected into the start of MinecraftClient.run()V
+@Mixin(Entity.class)
+public abstract class PlayerRayCastClientMixin {
+	@Shadow
+	public abstract World getWorld();
+
+	@Unique
+	private BlockPos lastPositionStaredAt;
+
+	@Unique
+	private static final Identifier INVENTORY_REQUEST = new Identifier("chest-tooltips", "inventory-request");
+
+	@Inject(at = @At("RETURN"), method = "raycast")
+	private void onRayCast(CallbackInfoReturnable<HitResult> callbackInfoReturnable) {
+		if (callbackInfoReturnable.getReturnValue() instanceof BlockHitResult hitResult) {
+			BlockPos blockPosition = hitResult.getBlockPos();
+
+			// Don't send network request if we have already requested container's contents.
+			if (blockPosition.equals(lastPositionStaredAt)) {
+				return;
+			}
+
+			lastPositionStaredAt = blockPosition;
+			BlockEntity blockEntity = this.getWorld().getBlockEntity(blockPosition);
+
+			if (blockEntity instanceof LootableContainerBlockEntity || blockEntity instanceof EnderChestBlockEntity) {
+				PacketByteBuf buffer = PacketByteBufs.create();
+				buffer.writeBlockPos(blockPosition);
+				ClientPlayNetworking.send(INVENTORY_REQUEST, buffer);
+			}
+		}
 	}
 }
