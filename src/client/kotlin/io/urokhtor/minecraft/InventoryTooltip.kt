6826e45dead76867935169f8f57b0037bd21d167
Urokhtor <urokhtor1@gmail.com>
2024-01-12 20:10:30 +0200

    Render the items neatly inside the tooltip

--- src/client/kotlin/io/urokhtor/minecraft/InventoryTooltip.kt
+++ src/client/kotlin/io/urokhtor/minecraft/InventoryTooltip.kt
@@ -5,37 +5,25 @@ import net.minecraft.client.gui.DrawContext
 import net.minecraft.item.AirBlockItem
 import net.minecraft.text.Text
 
-/**
- * Hovered tooltip positioner offsets tooltip 12 pixels up, and tooltip background 3 pixels more. This constant is used
- * to reset the offset.
- */
-private const val TOOLTIP_Y_OFFSET: Int = 15
-
-/**
- * Hovered tooltip positioner offsets tooltip text 12 pixels to the right, and tooltip background subtracts 3 pixels.
- * This constant is used to align the tooltip border with inventory background texture.
- */
-private const val TOOLTIP_X_OFFSET: Int = 9
+private const val Y_START: Int = 10
 
 class InventoryTooltip {
     fun render(textRenderer: TextRenderer, horizontalCenter: Int, drawContext: DrawContext, container: Container) {
+        val isInventoryEmpty = container.inventory
+            .none { it.item !is AirBlockItem }
+
+        if (isInventoryEmpty) {
+            drawContext.drawTooltip(
+                textRenderer,
+                Text.of(container.name),
+                horizontalCenter - textRenderer.getWidth(container.name),
+                Y_START + textRenderer.fontHeight
+            )
+            return
+        }
+
         val inventoryTooltipComponent = InventoryTooltipComponent(container)
         val xStart = horizontalCenter - (inventoryTooltipComponent.getItemsOnOneRow() / 2.0 * ITEM_SIZE_X).toInt()
-
-        val tooltipBottomPosition = TOOLTIP_Y_OFFSET + textRenderer.fontHeight
-
-        drawContext.drawTooltip(
-            textRenderer,
-            Text.of(container.name),
-            xStart - TOOLTIP_X_OFFSET,
-            tooltipBottomPosition
-        )
-
-        val inventoryHasItems = container.inventory
-            .any { it.item !is AirBlockItem }
-
-        if (inventoryHasItems) {
-            inventoryTooltipComponent.drawItems(textRenderer, xStart, tooltipBottomPosition, drawContext)
-        }
+        inventoryTooltipComponent.drawItems(textRenderer, xStart, Y_START, drawContext)
     }
 }
--- src/client/kotlin/io/urokhtor/minecraft/InventoryTooltipComponent.kt
+++ src/client/kotlin/io/urokhtor/minecraft/InventoryTooltipComponent.kt
@@ -2,13 +2,16 @@ package io.urokhtor.minecraft
 
 import net.minecraft.client.font.TextRenderer
 import net.minecraft.client.gui.DrawContext
+import net.minecraft.client.gui.tooltip.TooltipBackgroundRenderer
 import net.minecraft.client.gui.tooltip.TooltipComponent
+import net.minecraft.util.Colors
 import net.minecraft.util.Identifier
 import kotlin.math.roundToInt
 
 private const val MAX_ROW_LENGTH: Int = 9
 const val ITEM_SIZE_X: Int = 18
 private const val ITEM_SIZE_Y: Int = 20
+private const val PADDING: Int = 3
 private val BACKGROUND_TEXTURE = Identifier("container/bundle/background")
 private val SLOT_TEXTURE = Identifier("container/bundle/slot")
 
@@ -34,10 +37,23 @@ class InventoryTooltipComponent(private val container: Container) : TooltipCompo
     fun getItemsOnOneRow() = if (container.inventory.size < 9) container.inventory.size else MAX_ROW_LENGTH
 
     override fun drawItems(textRenderer: TextRenderer, x: Int, y: Int, drawContext: DrawContext) {
+        TooltipBackgroundRenderer.render(
+            drawContext,
+            x,
+            y,
+            getColumnsWidth(),
+            getRowsHeight() + textRenderer.fontHeight + PADDING,
+            0
+        )
+
+        drawContext.drawText(textRenderer, container.name, x, y, Colors.WHITE, true)
+
+        val backgroundTextureYStart = y + textRenderer.fontHeight + PADDING
+
         drawContext.drawGuiTexture(
             BACKGROUND_TEXTURE,
             x,
-            y,
+            backgroundTextureYStart,
             this.getColumnsWidth(),
             this.getRowsHeight()
         )
@@ -46,7 +62,7 @@ class InventoryTooltipComponent(private val container: Container) : TooltipCompo
             .forEachIndexed { stackIndex, itemStacks ->
                 itemStacks.forEachIndexed { itemIndex, itemStack ->
                     val xOffset = x + itemIndex * ITEM_SIZE_X + 1
-                    val yOffset = y + stackIndex * ITEM_SIZE_Y + 1
+                    val yOffset = backgroundTextureYStart + stackIndex * ITEM_SIZE_Y + 1
                     drawContext.drawGuiTexture(SLOT_TEXTURE, xOffset, yOffset, 0, ITEM_SIZE_X, ITEM_SIZE_Y)
                     drawContext.drawItem(itemStack, xOffset + 1, yOffset + 1)
                     drawContext.drawItemInSlot(textRenderer, itemStack, xOffset + 1, yOffset + 1)
