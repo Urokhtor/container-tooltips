aaeb285cc862c77bafaae11037fcef2fca500fab
Urokhtor <urokhtor1@gmail.com>
2024-08-10 13:21:28 +0300

    Add support for configuring which key shows the tooltip

--- src/client/java/io/urokhtor/minecraft/containertooltips/configuration/Configuration.java
+++ src/client/java/io/urokhtor/minecraft/containertooltips/configuration/Configuration.java
@@ -2,11 +2,15 @@ package io.urokhtor.minecraft.containertooltips.configuration;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import org.lwjgl.glfw.GLFW;
 
 @Config(name = "container-tooltips")
 public final class Configuration implements ConfigData {
 
-    @Comment("When on, tooltip is shown automatically. Otherwise, Left Shift must be pressed to show the tooltip.")
+    @ConfigEntry.Gui.Tooltip(count = 2)
     public boolean showAutomatically = true;
+
+    @ConfigEntry.Gui.Tooltip(count = 2)
+    public int showWithKeyCode = GLFW.GLFW_KEY_LEFT_SHIFT;
 }
