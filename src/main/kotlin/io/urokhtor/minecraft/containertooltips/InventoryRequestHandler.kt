19eba4a51c90136f4cb5784316c0d1f724b4f5d9
Urokhtor <urokhtor1@gmail.com>
2026-04-19 10:08:55 +0300

    Remove duplication when reading generic containers

--- src/main/kotlin/io/urokhtor/minecraft/containertooltips/InventoryRequestHandler.kt
+++ src/main/kotlin/io/urokhtor/minecraft/containertooltips/InventoryRequestHandler.kt
@@ -7,12 +7,11 @@ import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.block.ChestBlock
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity
+import net.minecraft.world.level.block.entity.BaseContainerBlockEntity
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.entity.ChestBlockEntity
 import net.minecraft.world.level.block.entity.EnderChestBlockEntity
 import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity
-
-fun RandomizableContainerBlockEntity.hasNotBeenLooted() = this.lootTable != null
 
 class InventoryRequestHandler {
 
@@ -24,7 +23,7 @@ class InventoryRequestHandler {
             is ChestBlockEntity -> readChestInventory(blockEntity, player)
             is EnderChestBlockEntity -> readEnderChestInventory(player)
             is RandomizableContainerBlockEntity -> readGenericInventory(blockEntity)
-            is AbstractFurnaceBlockEntity -> readFurnaceInventory(blockEntity)
+            is AbstractFurnaceBlockEntity -> blockEntity.readInventory()
             else -> null
         }
     }
@@ -74,28 +73,21 @@ class InventoryRequestHandler {
             return null
         }
 
-        val defaultedList = NonNullList.withSize(blockEntity.count(), ItemStack.EMPTY)
-        IntRange(0, blockEntity.count() - 1).forEach { slot ->
-            defaultedList[slot] = blockEntity.getItem(slot)
-        }
+        return blockEntity.readInventory()
+    }
+}
 
-        return InventoryResponsePayload(
-            name = blockEntity.displayName.getString(32),
-            maxSize = blockEntity.count(),
-            items = defaultedList
-        )
+fun RandomizableContainerBlockEntity.hasNotBeenLooted() = this.lootTable != null
+
+private fun BaseContainerBlockEntity.readInventory(): InventoryResponsePayload {
+    val defaultedList = NonNullList.withSize(this.count(), ItemStack.EMPTY)
+    IntRange(0, this.count() - 1).forEach { slot ->
+        defaultedList[slot] = this.getItem(slot)
     }
 
-    private fun readFurnaceInventory(blockEntity: AbstractFurnaceBlockEntity): InventoryResponsePayload {
-        val defaultedList = NonNullList.withSize(blockEntity.count(), ItemStack.EMPTY)
-        IntRange(0, blockEntity.count() - 1).forEach { slot ->
-            defaultedList[slot] = blockEntity.getItem(slot)
-        }
-
-        return InventoryResponsePayload(
-            name = blockEntity.displayName.getString(32),
-            maxSize = blockEntity.count(),
-            items = defaultedList
-        )
-    }
+    return InventoryResponsePayload(
+        name = this.displayName.getString(32),
+        maxSize = this.count(),
+        items = defaultedList
+    )
 }
